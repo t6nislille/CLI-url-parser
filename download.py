62f07d1e4e105429383ee5f8b9e875f50538487d
Tõnis <tonislille@gmail.com>
2025-12-16 14:33:50 +0200

    Replace retun with yieald in read_url

--- download.py
+++ download.py
@@ -7,13 +7,14 @@ def read_url(file_path):
     with open(file_path, "r", encoding="utf-8") as f:
         for line in f:
             name, url = line.strip().split("|")
-            return name, url
+            yield name, url
 
 # Make def main() into callable script
 @click.command()
 @click.option("-i", "--input-file", required=True, help="CSV file with URLs")
 def main(input_file):
-    pass
+    for name, url in read_url(input_file):
+        print(f"\"{name}\" URL = {url}")
 
 # Invoke main()
 if __name__ == "__main__":
